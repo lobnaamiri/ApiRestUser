200d542424ab66cf2be93a39888b546c8b1fa066
lobnaamiri <amirilobna2015@gmail.com>
2020-04-05 21:30:08 +0100

    adapter avec user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Entity\User;
+use App\Form\UserType;
+/**
+ * Product controller.
+ * @Route("/api", name="api_")
+ */
+class UserController extends FOSRestController
+{
+    /**
+     * Lists all Users.
+     * @Rest\Get("/users")
+     *
+     * @return Response
+     */
+    public function getUserAction()
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $users = $repository->findall();
+        return $this->handleView($this->view($users));
+    }
+    /**
+     * Create User.
+     * @Rest\Post("/user")
+     *
+     * @return Response
+     */
+    public function postUserAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+        }
+        return $this->handleView($this->view($form->getErrors()));
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="User")
+ */
+class User {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     *
+     */
+    private $username;
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     */
+    private $password;
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
